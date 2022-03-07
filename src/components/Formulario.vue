fdf22301ce15ad65f56f6e932d08f478cce30a60
Antonio Pires <tonan@outlook.com.br>
2022-03-07 07:19:55 -0300

    163. Two-way data binding (v-model)

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -8,19 +8,19 @@
                     <div class="mb-3 row">
                         <label class="col-3 col-form-label">Nome:</label>
                         <div class="col">
-                            <input type="text" class="form-control">
+                            <input type="text" class="form-control" v-model="nome">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label class="col-3 col-form-label">E-mail:</label>
                         <div class="col">
-                            <input type="email" class="form-control">
+                            <input type="email" class="form-control" v-model="email">
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label class="col-3 col-form-label">Senha:</label>
                         <div class="col">
-                            <input type="password" class="form-control">
+                            <input type="password" class="form-control" v-model="senha">
                         </div>
                     </div>
                     <div class="mb-3 row">
@@ -170,13 +170,13 @@
                 <span class="fs-4">SAÍDA DE DADOS</span>
                 <hr>
                 <div class="mb-3 row">
-                    <span>Nome:</span>
+                    <span>Nome: {{ nome }}</span>
                 </div>
                 <div class="mb-3 row">
-                    <span>E-mail:</span>
+                    <span>E-mail: {{ email }}</span>
                 </div>
                 <div class="mb-3 row">
-                    <span>Senha:</span>
+                    <span>Senha: {{ senha }}</span>
                 </div>
                 <div class="mb-3 row">
                     <span>Idade:</span>
@@ -229,6 +229,11 @@
 
 <script>
 export default {
-    name: 'Formulario'
+    name: 'Formulario',
+    data: () => ({
+        nome: '',
+        email: '',
+        senha:  ''
+    })
 }
 </script>

2c34bcc4381ddd9e006a497fedb30a703a1386bf
Antonio Pires <tonan@outlook.com.br>
2022-03-15 07:10:58 -0300

    170. Instalando e configurando a lib maska

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import Maska from 'maska'
+
+//createApp(App).mount('#app')
+
+const app = createApp(App);
+
+app.use(Maska);
+
+app.mount('#app');
